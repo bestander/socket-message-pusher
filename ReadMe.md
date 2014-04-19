8d9c46a9a756e8cd05894bf3760fe700fe0be68c
Konstantin Raev <bestander@gmail.com>
2014-04-20 00:06:33 +1200

    initial commit: started socket.io server with express. on POST message all socket clients get an async message

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+
+
+### to test from command line
+curl -X POST -H "Content-Type: application/json" -d '{"username":"xyz","password":"xyz"}' http://locaost:3000/push
---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var bodyParser = require('body-parser');
+
+app.use(bodyParser());
+
+server.listen(3000);
+
+app.post('/push', function (req, res) {
+    io.sockets.emit('message',req.body);
+    res.send("OK");
+});
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', "connected");
+});
